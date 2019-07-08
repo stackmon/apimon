df2fe747124ee5efe4526704c70e8e944643488f
Artem Goncharov <artem.goncharov@gmail.com>
2019-07-08 18:05:58 +0200

    fix lost changes

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,6 +3,8 @@
 mkdir -p /usr/app
 cd /usr/app
 
+checkout_location=test_repo
+
 chmod u+x executor.py
 
 if [[ ! "${EXECUTOR_REPO_URL}" ]]; then
@@ -12,20 +14,18 @@ else
     repo=${EXECUTOR_REPO_URL}
 fi
 
-git clone $repo api-monitoring
+git clone ${repo} ${checkout_location}
 
-cd api-monitoring
+if [ -f "{checkout_location}/requirements.yml" ]; then
+    ansible-galaxy install -r ${checkout_location}/requirements.yml
+fi
 
-git pull
-
-ansible-galaxy install -r requirements.yml
-
-work_dir=/usr/app/api_monitoring
+work_dir=/usr/app/${checkout_location}
 git_ref=${EXECUTOR_GIT_REF:-master}
 scenarios_location=${EXECUTOR_SCENARIOS_LOCATION:-playbooks/scenarios}
 interval=${EXECUTOR_REFRESH_INTERVAL:-120}
 count_executors=${EXECUTOR_COUNT_EXECUTORS:-10}
 
-../executor.py ${repo} --work_dir ${work_dir} --ref ${git_ref} --location ${scenarios_location} --interval ${interval} --count_executor ${count_executors}
+./executor.py ${repo} --work_dir ${work_dir} --ref ${git_ref} --location ${scenarios_location} --interval ${interval} --count_executor ${count_executors}
 
 wait
