f48f317cc4be130cbc723a5813e68bbbbd72d045
Artem Goncharov <Artem.goncharov@gmail.com>
2019-09-09 12:09:21 +0200

    Let the executor does everything itself

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,29 +5,8 @@ cd /usr/app
 
 git_dir=/usr/app/test_repo
 
-if [[ ! "${EXECUTOR_REPO_URL}" ]]; then
-    echo -e "\$EXECUTOR_REPO_URL variable must be set.\n"
-    exit 1
-else
-    repo=${EXECUTOR_REPO_URL}
-fi
-
-git clone ${repo} ${git_dir} --recurse-submodules
-
-if [ -f "${git_dir}/requirements.yml" ]; then
-    ansible-galaxy install -r ${git_dir}/requirements.yml
-fi
-
 config=${EXECUTOR_CONFIG:-/etc/apimon_executor/executor.yaml}
-git_ref=${EXECUTOR_GIT_REF:-master}
-scenarios_location=${EXECUTOR_SCENARIOS_LOCATION:-playbooks/scenarios}
-interval=${EXECUTOR_REFRESH_INTERVAL:-120}
-count_executors=${EXECUTOR_COUNT_EXECUTORS:-10}
 
-apimon-scheduler --config ${config} --repo ${repo} \
-    --git-checkout-dir ${git_dir} \
-    --git-ref ${git_ref} --location ${scenarios_location} \
-    --git-refresh-interval ${interval} \
-    --count-executor-threads ${count_executors}
+apimon-scheduler --config ${config}
 
 wait
