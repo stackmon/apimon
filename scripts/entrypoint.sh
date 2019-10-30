0339714bb536596bd27023eb22d9af7023990022
Artem Goncharov <artem.goncharov@gmail.com>
2019-10-30 11:24:29 +0100

    update entrypoint

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,22 +3,6 @@
 mkdir -p /usr/app
 cd /usr/app
 
-git_dir=/usr/app/test_repo
-
-if [[ ! "${EXECUTOR_REPO_URL}" ]]; then
-	 echo -e "\$EXECUTOR_REPO_URL variable must be set.\n"
-	 exit 1
-else
-   repo=${EXECUTOR_REPO_URL}
-fi
-	
-git clone ${repo} ${git_dir} --recurse-submodules
-	
-if [ -f "${git_dir}/requirements.yml" ]; then
-   ansible-galaxy install -r ${git_dir}/requirements.yml
-fi
-	
-
 config=${EXECUTOR_CONFIG:-/etc/apimon_executor/executor.yaml}
 
 apimon-scheduler --config ${config}
