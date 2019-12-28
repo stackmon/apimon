ff712f79b51242469994dc806cc1d75aa9615794
Artem Goncharov <artem.goncharov@gmail.com>
2019-12-28 16:15:07 +0100

    try to propagate signals to child process in entry point

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,8 +1,37 @@
 #!/bin/bash
 
+prep_term()
+{
+    unset term_child_pid
+    unset term_kill_needed
+    trap 'handle_term' TERM INT
+}
+
+handle_term()
+{
+    if [ "${term_child_pid}" ]; then
+        kill -TERM "${term_child_pid}" 2>/dev/null
+    else
+        term_kill_needed="yes"
+    fi
+}
+
+wait_term()
+{
+    term_child_pid=$!
+    if [ "${term_kill_needed}" ]; then
+        kill -TERM "${term_child_pid}" 2>/dev/null
+    fi
+    wait ${term_child_pid}
+    trap - TERM INT
+    wait ${term_child_pid}
+}
+
 mkdir -p /usr/app
 cd /usr/app
 
 config=${EXECUTOR_CONFIG:-/etc/apimon_executor/executor.yaml}
 
-apimon-scheduler --config ${config}
+prep_term
+apimon-scheduler --config ${config} &
+wait_term
